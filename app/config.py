500d5e25c247d5106011b51d4ad07fb2c203fe24
armandossrecife <armando.sousa@gmail.com>
2024-09-06 12:44:15 -0300

    Foram criadas as variaveis de ambiente para o banco

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
-USERNAME='armando'
-PASSWORD='skyinfo'
+import os
+
+my_username = os.environ.get('MYSQL_USERNAME')
+my_password = os.environ.get('MYSQL_PASSWORD')
+
+USERNAME=my_username
+PASSWORD=my_password
 HOST='localhost'
 PORT='3306'
 DATABASE_NAME='mydbteste'
